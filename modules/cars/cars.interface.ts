8aee2beedcb49bea9d5a0b17fc1f19f674fa8a1a
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-24 06:54:46 +0600

    first magic from ts

--- modules/cars/cars.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Schema, model, connect } from 'mongoose';
-
-export type Student = {
-    name: string;
-    email: string;
-    avatar?: string;
-  }
---
+++ src/app/modules/cars/cars.interface.ts
@@ -0,0 +1,21 @@
+import { Schema, model, connect } from 'mongoose';
+
+export enum CarCategory {
+    Sedan = 'Sedan',
+    SUV = 'SUV',
+    Truck = 'Truck',
+    Coupe = 'Coupe',
+    Convertible = 'Convertible',
+  }
+
+export type Student = {
+    name: string;
+    brand: string;
+    model: string;
+    year: number;
+    price: number;
+    category: CarCategory;
+    description: string;
+    quantity: number;
+    inStock: boolean;
+  }
